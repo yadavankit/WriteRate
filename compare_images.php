df5a8b4817eb40d30597a60341210dd04bfdc477
Ekta3012 <ekta30121995@gmail.com>
2016-04-03 13:50:54 +0530

    fifth commit

--- compare_images.php
+++ compare_images.php
@@ -4,19 +4,32 @@
     require 'connect.php';
     $comparador = new comparadorImg(8); 
 
-    /*for($i=0;$i<=4;$i++)
+    for($i=0;$i<=4;$i++)
     {
-      $alphabet.$i=$_SESSION['alphabet'.$i]; 
-      $f1 = 'ideal_img/'.$alphabet..'.jpg'; 
-      $f2 = 'photo/'.$alphabet.'.jpg'; 
-    }*/
-    $alphabet=$_SESSION['alphabet'];
-    /*$alphabet1=$_SESSION['alphabet1'];
+        $al= "alphabet".$i;
+        //echo $al;
+      $alphabet=$_SESSION[$al]; 
+       $f1 = 'ideal_img/'.$alphabet.'.jpg'; 
+       $f2 = 'photo/'.$alphabet.'.jpg'; 
+       //var_dump($f1);
+       //var_dump($f2);
+      $hash = $comparador->getHash_img($f1); 
+       echo $hash; 
+       $dif = $comparador->comparar_imgs($f1,$f2); 
+    
+       echo "<br><img src='".$f1."'><br><br>"; 
+       echo "<img src='".$f2."'><br><br>"; 
+        
+       echo "<b>Diferencias</b> ".$dif."%<br>"; 
+      echo "<b>Similitudes</b> ".(100-$dif)."%<br>";
+    }
+    /*$alphabet=$_SESSION['alphabet'];
+    $alphabet1=$_SESSION['alphabet1'];
     $alphabet2=$_SESSION['alphabet2'];
     $alphabet3=$_SESSION['alphabet3'];
     $alphabet4=$_SESSION['alphabet4'];*/
-    $f1 = 'ideal_img/'.$alphabet.'.jpg'; 
-    $f2 = 'photo/'.$alphabet.'.jpg';
+    //$f1 = 'ideal_img/'.$alphabet.'.jpg'; 
+    //$f2 = 'photo/'.$alphabet.'.jpg';
     /*$f1 = 'ideal_img/'.$alphabet1.'.jpg'; 
     $f2 = 'photo/'.$alphabet1.'.jpg';
     $f1 = 'ideal_img/'.$alphabet2.'.jpg'; 
@@ -24,7 +37,7 @@
     $f1 = 'ideal_img/'.$alphabet3.'.jpg'; 
     $f2 = 'photo/'.$alphabet3.'.jpg';
     $f1 = 'ideal_img/'.$alphabet4.'.jpg'; 
-    $f2 = 'photo/'.$alphabet4.'.jpg'; */
+    $f2 = 'photo/'.$alphabet4.'.jpg'; 
    // var_dump($f1);
     $hash = $comparador->getHash_img($f1); 
     echo $hash; 
@@ -34,6 +47,6 @@
     echo "<img src='".$f2."'><br><br>"; 
         
     echo "<b>Diferencias</b> ".$dif."%<br>"; 
-    echo "<b>Similitudes</b> ".(100-$dif)."%<br>"; 
+    echo "<b>Similitudes</b> ".(100-$dif)."%<br>"; */
     
 ?> 
